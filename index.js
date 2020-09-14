e7f7154a682b9f563d4a8fb77b14597c59eb17bf
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-09-14 11:23:12 +0200

    bug fix

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const Sheet = require('./sheet')
 
-(async function() {
-
-    const res = await fetch(
-      "https://www.investing.com/equities/apple-computer-inc"
-    );
+async function getPrice(url) {
+    const res = await fetch(url);
     const text = await res.text();
     const $ = cheerio.load(text);
     const price = $("#quotes_summary_current_data span").first().text();
-    console.log(price);
-    
-    
+    return price;
+}
+
+(async function() {
+    const sheet = new Sheet();
+    await sheet.load();
+    const stocks = await sheet.getRows(0);
+    const dayPrices = {};
+    for (let stock of stocks) {
+        const price = await getPrice(stock.url);
+        dayPrices[stock.ticker] = price;
+    }    
+    dayPrices.date = new Date().toDateString();
+    await sheet.addRows([dayPrices], 1)
 })();
--- sheet.js
+++ sheet.js
@@ -10,9 +10,14 @@ module.exports = class Sheet {
     await this.doc.useServiceAccountAuth(require("./credentials.json"));
     await this.doc.loadInfo();
   }
-  async addRows(rows) {
-    const sheet = this.doc.sheetsByIndex[0];
+  async addRows(rows, i) {
+    const sheet = this.doc.sheetsByIndex[i];
     await sheet.addRows(rows);
+  }
+  async getRows(i) {
+    const sheet = this.doc.sheetsByIndex[i];
+    const rows = await sheet.getRows(); // can pass in { limit, offset }
+    return rows;
   }
 }
 
